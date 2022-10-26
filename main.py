ba0d891632e70fc0fa804d4e0407a92a5a6b744f
Omeon <foshin99@bk.ru>
2022-10-26 13:08:54 +0300

    creature exe file

--- main.py
+++ main.py
@@ -1,20 +1,22 @@
 import PySimpleGUI as sg
 from counter import Counter
 from psgtray import SystemTray
+import os
+import sys
 
 
 def image_gui():
     sg.theme('dark grey 13')
 
     count_col = [[sg.Text('Левая кнопка: '), sg.Text(key='left')],
-                [sg.Text('Правая кнопка: '), sg.Text(key='right')],
-                [sg.Text('Средняя кнопка: '), sg.Text(key='middle')],
-                [sg.Text('Первая боковая кнопка: '), sg.Text(key='x1')],
-                [sg.Text('Вторая боковая кнопка: '), sg.Text(key='x2')]]
-    gif_col = [[sg.Image('monkey_mouse.gif', key='gif')]]
+                 [sg.Text('Правая кнопка: '), sg.Text(key='right')],
+                 [sg.Text('Средняя кнопка: '), sg.Text(key='middle')],
+                 [sg.Text('Первая боковая кнопка: '), sg.Text(key='x1')],
+                 [sg.Text('Вторая боковая кнопка: '), sg.Text(key='x2')]]
+    gif_col = [[sg.Image(resource_path('img/monkey_mouse.gif'), key='gif')]]
 
     layout = [[sg.Text('Это ClickCounter.', font=('Monospace', 20))],
-              [sg.Text('Посмотри, как сильно ты мучаете кнопки своей', font=('Monospace', 12))],
+              [sg.Text('Посмотри, как сильно ты мучаешь кнопки своей', font=('Monospace', 12))],
               [sg.Text('верной серой подруги.', font=('Monospace', 12))],
               [sg.Column(count_col), sg.Column(gif_col, justification='c')],
               [sg.Multiline(size=(55, 6), disabled=True, reroute_stdout=False, reroute_cprint=True, key='-OUT-')],
@@ -25,9 +27,9 @@ def image_gui():
                  ['Show window', 'Exit']
                  ]
 
-    window = sg.Window('ClickCounter', layout, finalize=True, icon='mouseclick.ico')
+    window = sg.Window('ClickCounter', layout, finalize=True, icon=resource_path('img/mouseclick.ico'))
 
-    tray = SystemTray(menu_tray, window=window, icon='mouseclick.ico',
+    tray = SystemTray(menu_tray, window=window, icon=resource_path('img/mouseclick.ico'),
                       tooltip='ClickCounter', single_click_events=False)
 
     # creating a mouse click monitoring stream
@@ -60,7 +62,7 @@ def image_gui():
         elif event == 'Exit':
             break
 
-        window['gif'].update_animation('monkey_mouse.gif', time_between_frames=50)
+        window['gif'].update_animation(resource_path('img/monkey_mouse.gif'), time_between_frames=50)
 
     window.close()
     tray.close()
@@ -70,6 +72,13 @@ def refresh_count(window: sg.Window, keys: list):
     for key in keys:
         if Counter.counter[key][1]:
             window[key](Counter.counter[key][1])
+
+
+def resource_path(relative):
+    if hasattr(sys, '_MEIPASS'):
+        return os.path.join(sys._MEIPASS, relative)
+    else:
+        return os.path.join(os.path.abspath("."), relative)
 
 
 def main():
